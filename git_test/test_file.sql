d0e4730b68b0dc3a726b645707bbd01ccd66659e
lipeiyi <lipeiyi@kuaishou.com>
2018-04-21 13:46:29 +0800

    选出日期，修改日期，删除一行

--- git_test/test_file.sql
+++ git_test/test_file.sql
@@ -1,9 +1,9 @@
-SELECT author_id,
+SELECT author_id,lpy_dt
        sum(if(dt="{{ds}}" and source='hot', report_cnt, 0)) hot_oneday_report,
        sum(if((dt BETWEEN date_sub("{{ds}}", 6) AND "{{ds}}") and source='hot', report_cnt, 0)) hot_sevenday_report,
        sum(if(source='hot',report_cnt,0)) hot_thirtyday_report,
        sum(if(dt="{{ds}}" and source='follow', report_cnt, 0)) follow_oneday_report,
-       sum(if((dt BETWEEN date_sub("{{ds}}", 10) AND "{{ds}}") and source='follow', report_cnt, 0)) follow_sevenday_report,
+       sum(if((dt BETWEEN date_sub("2018-04-21", 10) AND "2018-04-21") and source='follow', report_cnt, 0)) follow_sevenday_report,
        sum(if(source='follow',report_cnt,0)) follow_thirtyday_report,
        sum(if(dt="{{ds}}" and source='nearby', report_cnt, 0)) nearby_oneday_report,
        sum(if((dt BETWEEN date_sub("{{ds}}", 6) AND "{{ds}}") and source='nearby', report_cnt, 0)) nearby_sevenday_report,
@@ -23,7 +23,6 @@ SELECT author_id,
        sum(if(source='nearby',low_report_cnt,0)) nearby_thirtyday_low_report,
        sum(if(dt='2018-03-10' and (source is null or source not in ('hot','follow','nearby')), low_report_cnt, 0)) other_oneday_low_report,
        sum(if((dt BETWEEN date_sub("{{ds}}", 6) AND "{{ds}}") and (source is null or source not in ('hot','follow','nearby')), low_report_cnt, 0)) other_sevenday_low_report,
-       sum(if((source is null or source not in ('hot','follow','nearby')),low_report_cnt,0)) other_thirtyday_low_report,
 
        sum(if(dt='2018-03-10' and source='hot', low_report_cnt, 0)) hot_oneday_low_report,
        sum(if((dt BETWEEN date_sub("{{ds}}", 6) AND "{{ds}}") and source='hot', low_report_cnt, 0)) hot_sevenday_low_report,
@@ -66,4 +65,5 @@ SELECT author_id,
        sum(if((source is null or source not in ('hot','follow','nearby')),minor_improper_report_cnt,0)) other_thirtyday_minor_improper_report     
 FROM reco.author_report_aggr_daily
 WHERE (dt BETWEEN date_sub("{{ds}}", 29) AND "{{ds}}")
-GROUP BY author_id 
+GROUP BY author_id
+ 
